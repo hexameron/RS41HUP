a65348e2d28bcd1154e9523906261e3a0ae16a86
hexameron <hexameron>
2018-08-17 19:17:59 +0100

    Refactor.

--- contestia.c
+++ contestia.c
@@ -1,6 +1,6 @@
 /*
- * Outputs contestia to the si_trx
- * Copyright (C) 2015  Richard Meadows <richardeoin>
+ * Functions for MFSK encoding / decoding
+ * Copyright (C) 2014  Richard Meadows <richardeoin>
  *
  * Permission is hereby granted, free of charge, to any person obtaining
  * a copy of this software and associated documentation files (the
@@ -24,85 +24,153 @@
 
 #include <string.h>
 
+#include "mfsk.h"
+#include "fwht.h"
 #include "contestia.h"
 
-#define PREAMBLE_LENGTH CONTESTIA_NUMBER_OF_TONES
+static const uint64_t scrambler_olivia		= 0xE257E6D0291574ECLL;
+static const uint64_t scrambler_contestia	= 0xEDB88320LL;
 
 /**
- * Current output tones
- */
-int8_t contestia_tones[CONTESTIA_NUMBER_OF_TONES];
-/**
- * Where we are in the current output tones
- */
-uint32_t contestia_tone_index = 0xFFFFFFFE;
-uint8_t contestia_preamble_index = 0;
-
-/**
- * Starts the transmission of a contestia block
- */
-void contestia_start(char* block) {
-  /* Start transmission */
-  contestia_mfsk_encode_block(block, contestia_tones);
-  contestia_tone_index = 0;
-}
-void contestia_preamble(void) {
-  contestia_preamble_index = PREAMBLE_LENGTH;
-}
-
-void contestia_set_tone(uint8_t tone) {
-  /* Align this to a channel */
-  //int16_t channel = tone - (CONTESTIA_NUMBER_OF_TONES / 2);
-  //si_trx_switch_channel(channel * CONTESTIA_CHANNEL_SPACING);
-}
-
-/**
- * Called at the baud rate, outputs tones
+ * USEFUL RESOURES =============================================================
  *
- * Returns 1 when more work todo, 0 when finished
+ * http://en.wikipedia.org/wiki/Olivia_MFSK
+ * http://www.arrl.org/olivia
+ *
+ * Also, although without images:
+ * http://web.archive.org/web/20070927210543/http://homepage.sunrise.ch/mysunrise/jalocha/fht_coding.htm
+ *
+ * Useful guide to the differences between olivia and contestia
+ * (ultimately we'd like to support both):
+ * http://f1ult.free.fr/DIGIMODES/MULTIPSK/contestia_rttym_en.htm
+ *
+ * Also dl-fldigi's source might be helpful:
+ * https://github.com/jamescoxon/dl-fldigi/blob/master/src/include/jalocha/pj_mfsk.h
  */
-uint8_t contestia_tick(void) {
 
-  if (contestia_preamble_index) {
-    contestia_preamble_index--;
+void mfsk_encode_block(char* block, int8_t* tones,
+                       uint8_t symbols_per_block,	/* The number of on-the-air symbols to transmit for each block */
+                       uint8_t bits_per_symbol,		/* The number of bits encoded in each on-the-air symbol */
+                       uint64_t scrambler,		/* Scrambler sequence */
+                       uint8_t scrambling_shift)	/* Scrambler shift */
+{
+  int8_t fwht_vector[symbols_per_block];
+  memset(tones, 0, symbols_per_block * sizeof(int8_t));
 
-    if (contestia_preamble_index & (CONTESTIA_NUMBER_OF_TONES/4)) {
-      contestia_set_tone(CONTESTIA_NUMBER_OF_TONES - 1);
+  /**
+   * There is one bit in the symbol for each character in the
+   * block. Iterate over each character.
+   */
+  for (uint8_t character = 0; character < bits_per_symbol; character++) {
+
+    /* Mask off unuseds bits in the character */
+    block[character] &= ((symbols_per_block * 2) - 1);
+
+    /* Set a deviation in the input vector */
+    memset(fwht_vector, 0, symbols_per_block * sizeof(int8_t));
+    if (block[character] < symbols_per_block) {
+      fwht_vector[block[character] - 0]                  = 1;	/* +ve */
     } else {
-      contestia_set_tone(0);
+      fwht_vector[block[character] - symbols_per_block] = -1;	/* -ve */
     }
 
-    return 1;
+    /* Perform an in-place Inverse Fast Walsh-Hadamard Transform */
+    ifwht(fwht_vector, symbols_per_block);
+
+    /* Iterate over each symbol in the output block */
+    for (uint32_t symbol = 0, mask_index = character * scrambling_shift;
+         symbol < symbols_per_block;
+         symbol++, mask_index++) {
+
+      mask_index %= symbols_per_block;
+
+      /* If this bit in the FWHT is significant */
+      if ((scrambler & (1LL << mask_index)) ?
+          (fwht_vector[symbol] > 0) :	/* Scrambled:     +ve is significant */
+          (fwht_vector[symbol] < 0)) {	/* Not Scrambled: -ve is significant */
+
+        /* Find the bit index to set */
+        uint8_t bit_index = (character + symbol) % bits_per_symbol;
+
+        /* Set this bit */
+        tones[symbol] |= (1 << bit_index);
+      }
+    }
   }
-
-  if (contestia_tone_index < CONTESTIA_NUMBER_OF_TONES) {
-    uint8_t binary_code;
-    uint8_t grey_code;
-
-    /* Output grey code */
-    binary_code = contestia_tones[contestia_tone_index];
-    grey_code = (binary_code >> 1) ^ binary_code;
-
-    /* Transmit this tone */
-    contestia_set_tone(grey_code);
-
-  } else {
-    return 0;
-  }
-
-  contestia_tone_index++;
-
-  if (contestia_tone_index < CONTESTIA_NUMBER_OF_TONES) {
-    return 1;
-  }
-
-  return 0;
 }
 
-uint8_t contestia_convert(char* returnstring) {
-	uint8_t index, outdex, shift, binarycode, greycode;
 
-	// 32 tones of 2 bits each => 8 bytes per two chars input
+/**
+ * This function encodes a single block of Olivia MFSK
+ *
+ * It takes a buffer of ASCII-encoded text and returns an array of
+ * tones to transmit.
+ */
+void olivia_mfsk_encode_block(char* block, int8_t* tones)
+{
+  size_t bits_per_symbol = 2; /* That is, there are 2^2=4 tones */
+
+  mfsk_encode_block(block, tones, 64, bits_per_symbol, scrambler_olivia, 13);
+}
+/**
+ * This function encodes a single block of Contestia MFSK
+ *
+ * It takes a buffer of ASCII-encoded text and returns an array of
+ * tones to transmit.
+ */
+void contestia_mfsk_encode_block(char* block, int8_t* tones)
+{
+  size_t bits_per_symbol = 2; /* That is, there are 2^2=4 tones */
+
+  for (uint8_t c_index = 0; c_index < bits_per_symbol; c_index++) {
+    char character = block[c_index];
+
+    /* lowercase => UPPERCASE */
+    if (character >= 'a' && character <= 'z') {
+      character += 'A' - 'a';
+    }
+
+    /* Convert to contestia character set */
+    if (character >= '!' && character <= 'Z') {	/* Printables...   */
+      character -= 32;
+    } else if (character == ' ') {		/* Space           */
+      character = 59;
+    } else if (character == '\r') {		/* Carriage Return */
+      character = 60;
+    } else if (character == '\n') {		/* Line Feed       */
+      character = 0;
+    } else if (character == 8) {		/* Backspace       */
+      character = 61;
+    } else if (character == 0) {		/* Null            */
+      character = 0;
+    } else {					/* ????????????    */
+      character = '?' - 32;
+    }
+
+    block[c_index] = character;
+  }
+
+
+  mfsk_encode_block(block, tones, 32, bits_per_symbol, scrambler_contestia, 5);
+}
+
+
+/*
+ *  Code to interface with Project Horus 4fsk code in main.c
+ */
+
+int8_t contestia_tones[CONTESTIA_NUMBER_OF_TONES];
+
+/* translate a block of 2 chars into 32 tones */
+void contestia_start(char* block) {
+	contestia_mfsk_encode_block(block, contestia_tones);
+}
+
+/* pack a block of 32 tones into 8 chars for transmission */
+uint8_t contestia_convert(char* returnstring) {
+	uint8_t index, outdex, shift;
+	int8_t	binarycode, greycode;
+
 	for (index = 0; index < CONTESTIA_NUMBER_OF_TONES; index++) {
 		binarycode = contestia_tones[index];
 		greycode = (binarycode >> 1) ^ binarycode;
@@ -118,11 +186,7 @@ uint8_t contestia_convert(char* returnstring) {
 }
 
 
-/**
- * Only certain strings are possible in contestia (no lowercase
- * etc.). This function prases a string and fixed anything that would
- * be converted by contestia transmission.
- */
+/* This function parses a string and converts to contestia characters */
 void contestiaize(char* string) {
   for (size_t i = 0; i < strlen(string); i++) {
 
